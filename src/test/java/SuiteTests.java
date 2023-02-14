7850c123029c5c5a2a3badb84d1185876ccfd542
Jungsun Kim <kimjs@hanyang.ac.kr>
2023-02-14 13:05:37 +0900

    pom.xml updated; jupiter downgraded

--- src/test/java/SuiteTests.java
+++
@@ -1,8 +0,0 @@
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-
-@SelectPackages({"video.rental.demo", "video.rental.demo.domain"})
-@Suite
-public class SuiteTests {
-
-}

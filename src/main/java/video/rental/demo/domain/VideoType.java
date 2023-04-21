3524c61647423c5689e522b47dfecf096c5e45bb
raegon <raegon@gmail.com>
2023-04-21 17:17:05 +0900

    VideoType

--- src/main/java/video/rental/demo/domain/VideoType.java
+++ src/main/java/video/rental/demo/domain/VideoType.java
@@ -1,15 +1,19 @@
 package video.rental.demo.domain;
 
+import java.util.Arrays;
+
 public enum VideoType {
 
-  VHS(1, 5),
-  CD(2, 3),
-  DVD(3, 2);
+  VHS(1, 1, 5),
+  CD(2, 2, 3),
+  DVD(3, 3, 2);
 
-  final int penalty;
-  final int limit;
+  private final int code;
+  private final int penalty;
+  private final int limit;
 
-  VideoType(int penalty, int limit) {
+  VideoType(int code, int penalty, int limit) {
+    this.code = code;
     this.penalty = penalty;
     this.limit = limit;
   }
@@ -22,16 +26,16 @@ public enum VideoType {
     return limit;
   }
 
-    public static VideoType from(int videoType) {
-    switch (videoType) {
-      case 1:
-        return VideoType.VHS;
-      case 2:
-        return VideoType.CD;
-      case 3:
-        return VideoType.DVD;
-    }
-    throw new IllegalArgumentException("Invalid type " + videoType);
+  public static VideoType from(int videoType) {
+    return Arrays.stream(VideoType.values())
+        .filter(type -> type.code == videoType)
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException("Invalid type " + videoType));
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(code);
   }
 
 }
